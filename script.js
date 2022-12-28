7fad91fc79a0d443772fd4ea5029a5fb053ff6eb
Centimeterbain <80384282+Centimeterbain@users.noreply.github.com>
2022-12-29 01:53:07 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// AOS動畫區
+AOS.init({
+  duration: 1200
+});
+// AOS動畫區
+
+import gasp from "https://cdn.skypack.dev/gasp@0.0.2";
+
+import * as gsap from "https://cdn.skypack.dev/gsap";
+
+
+gsap.registerPlugin(ScrollToPlugin, ScrollTrigger);
